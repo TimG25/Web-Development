7f5ab7bc1975385ea9dc2755dad5471c332c3b2a
TimG25 <Tim.geuens@student.vives.be>
2025-02-22 18:10:10 +0100

    labo 3 commit 5

--- Semester 2/Labo 3 Javascript/Opdracht Producten/scripts/code.js
+++ Semester 2/Labo 3 Javascript/Opdracht Producten/scripts/code.js
@@ -1,3 +1,31 @@
 const setup = () => {
+    let btnHerbereken = document.getElementById("btnHerbereken");
+
+    btnHerbereken.addEventListener("click", prijsBerekening);
 }
+
+const prijsBerekening = () => {
+    let prijzen = document.getElementsByClassName("prijs");
+    let aantallen = document.getElementsByClassName("aantal");
+    let btw = document.getElementsByClassName("btw");
+    let subtotaalElementen = document.getElementsByClassName("subtotaal")
+    let totaal = document.getElementById("totaal")
+
+    let totaalWaarde = 0;
+
+    for(let i = 0; i < prijzen.length; i++) {
+        let prijs = parseFloat(prijzen[i].innerHTML);
+        let btwPercentage = parseFloat(btw[i].innerHTML) / 100;
+        let aantal = parseFloat(aantallen[i].value);
+
+        let subtotaal = prijs * aantal * (1 + btwPercentage);
+
+        subtotaalElementen[i].innerHTML=subtotaal.toFixed(2) + ' Eur';
+
+        totaalWaarde += subtotaal;
+    }
+
+    totaal.innerHTML=totaalWaarde.toFixed(2) + ' Eur';
+}
+
 window.addEventListener("load", setup);
