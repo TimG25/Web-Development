896f7809003fd9bc938d390855bfd6f35dfce365
TimG25 <Tim.geuens@student.vives.be>
2025-02-24 16:22:39 +0100

    Labo 4 commit 5

--- Semester 2/Labo 4 Javascript/Opdracht spaties met functies/scripts/code.js
+++ Semester 2/Labo 4 Javascript/Opdracht spaties met functies/scripts/code.js
@@ -1,3 +1,29 @@
 const setup = () => {
+    let btnSplitsen = document.getElementById("btn");
+
+    btnSplitsen.addEventListener("click", Splitsen);
+}
+
+const maakMetSpaties = (inputText) => {
+    let result = "";
+
+    for (let i = 0; i < inputText.length; i++) {
+        if (inputText[i] !== " ") {
+            result += inputText[i] + " ";
+        } else {
+            result += "";
+        }
+    }
+
+    return result.trim();
+}
+
+
+const Splitsen = () => {
+    let inputText = document.getElementById("inputVeld").value;
+
+    let spacedText = maakMetSpaties(inputText);
+
+    console.log(spacedText);
 }
 window.addEventListener("load", setup);
