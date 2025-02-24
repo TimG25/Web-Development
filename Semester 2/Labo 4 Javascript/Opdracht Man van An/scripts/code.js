855a5d5c3795672afe89d336ab152c4ac0dd970e
TimG25 <Tim.geuens@student.vives.be>
2025-02-24 16:28:24 +0100

    Labo 4 commit 6

--- Semester 2/Labo 4 Javascript/Opdracht Man van An/scripts/code.js
+++ Semester 2/Labo 4 Javascript/Opdracht Man van An/scripts/code.js
@@ -1,3 +1,28 @@
 const setup = () => {
+    const tekst1 = "De man van An geeft geen hand aan ambetante verwanten";
+    const zoekTerm1 = "an";
+
+    let count1 = 0;
+    let index1 = tekst1.indexOf(zoekTerm1);
+
+    while (index1 !== -1) {
+        count1++;
+        index1 = tekst1.indexOf(zoekTerm1, index1 + 1);
+    }
+
+    console.log("Aantal keren 'an' gevonden met indexOf:", count1);
+
+    const tekst2 = "De man van An geeft geen hand aan ambetante verwanten";
+    const zoekTerm2 = "an";
+
+    let count2 = 0;
+    let index2 = tekst2.lastIndexOf(zoekTerm2);
+
+    while (index2 !== -1) {
+        count2++;
+        index2 = tekst2.lastIndexOf(zoekTerm2, index2 - 1);  // Zoek vanaf de vorige positie
+    }
+
+    console.log("Aantal keren 'an' gevonden met lastIndexOf:", count2);
 }
 window.addEventListener("load", setup);
