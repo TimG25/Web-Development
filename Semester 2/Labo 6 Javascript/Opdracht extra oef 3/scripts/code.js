9df4c8e387e51e2eead6bf318a0210c0c2d96fec
TimG25 <Tim.geuens@student.vives.be>
2025-03-18 16:57:09 +0100

    labo 6 commit 2

--- Semester 2/Labo 6 Javascript/Opdracht extra oef 3/scripts/code.js
+++ Semester 2/Labo 6 Javascript/Opdracht extra oef 3/scripts/code.js
@@ -1,3 +1,8 @@
 const setup = () => {
+    let newParagraph = document.createElement("p");
+    newParagraph.textContent = "This is a new paragraph";
+
+    let myDIV = document.getElementById("myDIV");
+    myDIV.appendChild(newParagraph);
 }
 window.addEventListener("load", setup);
