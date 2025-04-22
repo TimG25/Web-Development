72fdd8b7947f468c49ef0c3d8090f670b34d3363
TimG25 <Tim.geuens@student.vives.be>
2025-04-22 16:57:45 +0200

    Labo 9 commit 2

--- Semester 2/Labo 9 Javascript/Start_Colorpicker/Start_Colorpicker/scripts/storage.js
+++ Semester 2/Labo 9 Javascript/Start_Colorpicker/Start_Colorpicker/scripts/storage.js
@@ -1,18 +1,50 @@
 
 
 const storeSliderValues = () => {
+    let red;
+    let green;
+    let blue;
 
+    red = document.getElementById("sldRed").value;
+    green = document.getElementById("sldGreen").value;
+    blue = document.getElementById("sldBlue").value;
+
+    localStorage.setItem("red", red);
+    localStorage.setItem("green", green);
+    localStorage.setItem("blue", blue);
 };
 
 const restoreSliderValues = () => {
+    let red = localStorage.getItem("red");
+    let green = localStorage.getItem("green");
+    let blue = localStorage.getItem("blue");
 
+    document.getElementById("sldRed").value = red;
+    document.getElementById("sldGreen").value = green;
+    document.getElementById("sldBlue").value = blue;
 };
 
 const storeSwatches = () => {
     // bouw een array met kleurinfo objecten
+    let swatches = {};
+    let swatchesJSON;
 
+    swatches.red = document.getElementById("sldRed").value;
+    swatches.green = document.getElementById("sldGreen").value;
+    swatches.blue = document.getElementById("sldBlue").value;
+
+    swatchesJSON = JSON.stringify(swatches);
+    localStorage.setItem("swatches", swatchesJSON);
 };
 
 const restoreSwatches = () => {
+    let swatches;
+    let swatchesJSON = localStorage.getItem("swatches");
+
+    swatches = JSON.parse(swatchesJSON);
+
+    document.getElementById("sldRed").value = swatches.red;
+    document.getElementById("sldGreen").value = swatches.green;
+    document.getElementById("sldBlue").value = swatches.blue;
 
 };
